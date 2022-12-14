597a079b0168e06111dcf732e056aa979dc34a8d
Piotr <kozlowski.piotr3@gmail.com>
2022-12-14 17:26:25 +0100

    main function done, workin on print

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 class Nodes {
-    constructor(value, leftChild, rightChild) {
+    constructor(value, leftChild = null, rightChild = null) {
         this.value = value
         this.leftChild = leftChild
         this.rightChild = rightChild
@@ -12,14 +12,15 @@ class Tree {
     }
 
     buildTree(array = []) {
+        if (array.length === 0) return
+        
         const start = 0
-        const end = array.length - 1
-        const mid = end / 2
+        const end = array.length 
+        const mid = Math.round(end / 2)
 
-        if (array[start] > array[end]) return
-        const root = new Nodes(array[mid])
-        root.leftChild = this.buildTree(array.slice(start, mid))
-        root.rightChild = this.buildTree(array.slice(mid+1, end))
+        const root = new Nodes(array[mid-1])
+        root.leftChild = this.buildTree(array.slice(start, mid-1))
+        root.rightChild = this.buildTree(array.slice(mid, end))
 
         return root
     }
@@ -27,3 +28,15 @@ class Tree {
 
 const newTree = new Tree
 console.log(newTree.buildTree([1,2,3,4,5,6,7,8,9]))
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node.rightChild !== undefined) {
+      prettyPrint(node.rightChild, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.leftChild !== undefined) {
+      prettyPrint(node.leftChild, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+}
+
+prettyPrint(newTree.buildTree([1,2,3,4,5,6,7,8,9]))

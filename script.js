58496a99456a9a9cb18028cef45be8dd4914183d
Piotr <kozlowski.piotr3@gmail.com>
2022-12-18 13:45:33 +0100

    order and insert functions done

--- script.js
+++ script.js
@@ -22,12 +22,31 @@ class Tree {
         root.leftChild = this.buildTree(array.slice(start, mid-1))
         root.rightChild = this.buildTree(array.slice(mid, end))
 
-        return root
+        return this.root = root
+    }
+
+    order(newRoot = this.root, arr = []) {
+        if (newRoot.leftChild !== undefined) this.order(newRoot.leftChild, arr)
+        arr.push(newRoot.value)
+        if (newRoot.rightChild !== undefined) this.order(newRoot.rightChild, arr)
+        return arr
+    }
+
+    insert(value, newRoot = this.root, arr = []) {
+        if (newRoot.leftChild !== undefined) this.insert(value, newRoot.leftChild, arr)
+        if (value <= newRoot.value) {
+            arr.push(value)
+            arr.push(newRoot.value)
+            value = undefined
+        } else {
+            arr.push(newRoot.value)
+        }
+        if (newRoot.rightChild !== undefined) this.insert(value, newRoot.rightChild, arr)
+        return arr, this.buildTree(arr)
     }
 }
 
 const newTree = new Tree
-console.log(newTree.buildTree([1,2,3,4,5,6,7,8,9]))
 
 const prettyPrint = (node, prefix = '', isLeft = true) => {
     if (node.rightChild !== undefined) {
@@ -39,4 +58,15 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
     }
 }
 
-prettyPrint(newTree.buildTree([1,2,3,4,5,6,7,8,9]))
+
+newTree.buildTree([1,3,5,7,9,11,13,14,15,16,17,18])
+prettyPrint(newTree.root)
+console.log(newTree.root)
+console.log(newTree.insert(6))
+console.log(newTree.insert(8))
+console.log(newTree.insert(10))
+console.log(newTree.insert(12))
+
+
+prettyPrint(newTree.root)
+// [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
